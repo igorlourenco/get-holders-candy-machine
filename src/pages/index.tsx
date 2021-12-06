7438509cd004cb6fe33206380f1038645a97888a
Igor Lourenço <theigorlourenco@gmail.com>
2021-12-06 14:07:29 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Layout } from "../components/common/layout";
 import { HoldersList } from "../components/common/holders-list";
 import * as anchor from "@project-serum/anchor";
 
-const rpcHost = "https://api.mainnet-beta.solana.com";
+const rpcHost = "https://cool-winter-haze.solana-mainnet.quiknode.pro/4a52a079a306a036cd67383149f42bac92d3e7c9";
 const connection = new anchor.web3.Connection(rpcHost);
 
 export default function HoldersPage() {
