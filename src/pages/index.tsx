a4bf6cde687ecd77e20e217607d0629cb38ec603
Igor Lourenço <igorlourenco.dev@gmail.com>
2021-12-06 14:15:03 -0300

    feat; update node

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
-
 import { Stack, Text, Button } from "@chakra-ui/react";
 import { Layout } from "../components/common/layout";
 import { HoldersList } from "../components/common/holders-list";
 import * as anchor from "@project-serum/anchor";
 
-const rpcHost = "https://cool-winter-haze.solana-mainnet.quiknode.pro/4a52a079a306a036cd67383149f42bac92d3e7c9";
+const rpcHost = "https://cool-winter-haze.solana-mainnet.quiknode.pro";
 const connection = new anchor.web3.Connection(rpcHost);
 
 export default function HoldersPage() {
   return (
     <Layout>
       <Stack alignItems="center" py={20} spacing={4}>
-        <HoldersList connection={connection}/>
+        <HoldersList connection={connection} />
       </Stack>
     </Layout>
   );
