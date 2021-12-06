ab7025bb9b63e52eba7fe0bea80464783b2144ed
Igor Lourenço <theigorlourenco@gmail.com>
2021-12-06 14:29:07 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Layout } from "../components/common/layout";
 import { HoldersList } from "../components/common/holders-list";
 import * as anchor from "@project-serum/anchor";
 
-const rpcHost = "https://solana-mainnet.phantom.tech";
+const rpcHost = "https://bitter-ancient-violet.solana-mainnet.quiknode.pro/2a06d36e288a343cff964c14dde72a90b27c773c";
 const connection = new anchor.web3.Connection(rpcHost);
 
 export default function HoldersPage() {
